f389b5be36b0130d8ca3c68ca1d3e0238b10496e
pietrop <pietro.passarelli@gmail.com>
2017-06-07 16:28:28 +0800

    Replaced " with \'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var fs = require("fs");
+var fs = require('fs');
 var cp = require('child_process');
 
 var command = null; 
@@ -34,17 +34,17 @@ function updatedListOfFfmpegFormats(){
 	//the child process output gives 3 outputs, the middle one is our text content as returned by ffmpeg when running `ffmpeg -formats` from terminal
 	var listOfFormats = childProcess.output[1];
 	// the list header is seprated with a `--` symbol.  see example in `ffmpeg_formats_raw_extensions.txt`
-	listOfFormats = listOfFormats.split("--")[1];
+	listOfFormats = listOfFormats.split('--')[1];
 
 	//dividing into lines 
-	var linesListOfFormats = listOfFormats.split("\n");
+	var linesListOfFormats = listOfFormats.split('\n');
 
 	// remove all empty lines frm the array. Probably just first and last but doing this to be safe.
 	// https://stackoverflow.com/questions/19888689/remove-empty-strings-from-array-while-keeping-record-without-loop
 	linesListOfFormats = linesListOfFormats.filter(Boolean)
 
 	//feedback on number of lines identified
-	// console.log("Number of formats in list",linesListOfFormats.length);
+	// console.log('Number of formats in list',linesListOfFormats.length);
 
 	// iterate over the lines of formats 
 	linesListOfFormats.forEach(iterateOverLinesListFormats);
@@ -52,19 +52,19 @@ function updatedListOfFfmpegFormats(){
 	// helper funciton to iterate over the lines of formats
 	function iterateOverLinesListFormats(item, index,collection){
 		// split each line on space
-		var itemArray = item.split(" ")
+		var itemArray = item.split(' ')
 		//// eg 
 		//// [ '', 'DE', '', 'yuv4mpegpipe', '', '', 'YUV4MPEG', 'pipe' ]
 		var extension = itemArray[3];
 		// if meets validation add to list 
-		if(extension !== "" &&  extension !== undefined){
+		if(extension !== '' &&  extension !== undefined){
 			formatExtensionsReultsList.push(extension);
 			// however spacing is a bit of sometimes, so in that case extension is in second element
 			// eg 
 			// [ '','DE',  'wtv',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  '',  'Windows',  'Television','(WTV)' ]
-		}else if(extension === "" ){
+		}else if(extension === '' ){
 			 extension = itemArray[2];
-			 if(extension !== "" &&  extension !== undefined){
+			 if(extension !== '' &&  extension !== undefined){
 			 		formatExtensionsReultsList.push(extension);
 			 }
 		}
@@ -74,11 +74,11 @@ function updatedListOfFfmpegFormats(){
 
 
 	//resulting list count
-	// console.log("number of formats identified", formatExtensionsReultsList.length)
+	// console.log('number of formats identified', formatExtensionsReultsList.length)
 	// show preview 
 	// console.log(formatExtensionsReultsList)
 	//save json, array of list 
-	fs.writeFileSync("ffmpeg_extentions.json", JSON.stringify(formatExtensionsReultsList))
+	fs.writeFileSync('ffmpeg_extentions.json', JSON.stringify(formatExtensionsReultsList))
 
 
 	return formatExtensionsReultsList;
@@ -87,7 +87,7 @@ function updatedListOfFfmpegFormats(){
 
 
 module.exports = {
-    listOfFormats: require("./ffmpeg_extentions.json"),
+    listOfFormats: require('./ffmpeg_extentions.json'),
     updatedListOFFormats: updatedListOfFfmpegFormats,
     setffmpegPath: function(ffmpegPath){
     	setffmpegPath(ffmpegPath)
--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,42 +1,41 @@
 'use strict';
 
-var ffmpegFormatsListMaker = require("../index.js");
-var defaultListOfFormats = require("../ffmpeg_extentions.json");
+var ffmpegFormatsListMaker = require('../index.js');
+var defaultListOfFormats = require('../ffmpeg_extentions.json');
 
 //exampel paths for ffmpeg testing
-var examplePath = "/usr/local/bin/ffmpeg";
-var wrongExamplePath = "/some/random/made/up/path"
+var examplePath = '/usr/local/bin/ffmpeg';
+var wrongExamplePath = '/some/random/made/up/path'
 
-describe("ffmpeg format list module", function() {
-  it("should return a list of ffmpeg formats", function() {
-    expect(ffmpegFormatsListMaker.listOfFormats).toEqual(defaultListOfFormats);
-  });
+describe('ffmpeg format list module', function() {
 
-   it("should return an updated list of ffmpeg formats", function() {
-    expect(ffmpegFormatsListMaker.updatedListOFFormats()).toEqual(defaultListOfFormats);
-  });
+	it('should return a list of ffmpeg formats', function() {
+		expect(ffmpegFormatsListMaker.listOfFormats).toEqual(defaultListOfFormats);
+	});
 
+	it('should return an updated list of ffmpeg formats', function() {
+		expect(ffmpegFormatsListMaker.updatedListOFFormats()).toEqual(defaultListOfFormats);
+	});
 
-    it("should allow to set a path for ffmpeg", function() {
+	it('should allow to set a path for ffmpeg', function() {
     	//set path 
     	ffmpegFormatsListMaker.setffmpegPath(examplePath)
 
     	expect(examplePath).toEqual(ffmpegFormatsListMaker.returnffmpegPath());
-  
-    	expect(wrongExamplePath).not.toEqual(ffmpegFormatsListMaker.returnffmpegPath());
-  });
 
+    	expect(wrongExamplePath).not.toEqual(ffmpegFormatsListMaker.returnffmpegPath());
+    });
 
     //this cover use cases when packaging module inside of nwjs or electron type of apps
-   it("should return formats even with a custom path set for ffmpeg", function() {
+    it('should return formats even with a custom path set for ffmpeg', function() {
     	//set path 
     	ffmpegFormatsListMaker.setffmpegPath(examplePath);
     	//only testing generated list, coz defaults reads from json 
-   	 	expect(ffmpegFormatsListMaker.updatedListOFFormats()).toEqual(defaultListOfFormats);
-   	});
+    	expect(ffmpegFormatsListMaker.updatedListOFFormats()).toEqual(defaultListOfFormats);
+    });
 
    //TODO: to do this test need better validare of whether a valid path for ffmpeg has been provided
-    // it("should not return formats if the a custom path provided for for ffmpeg in invalid", function() {
+    // it('should not return formats if the a custom path provided for for ffmpeg in invalid', function() {
     	
     // 	//set path 
     // 	ffmpegFormatsListMaker.setffmpegPath(wrongExamplePath);
